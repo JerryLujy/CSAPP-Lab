020ed37d90c2a49d0f50ade90af5da745fab853c
Jieyu Lu <jerrylujieyu@gmail.com>
2015-10-13 14:04:02 -0400

    basic data structure

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -3,6 +3,16 @@
 #include <unistd.h>
 #include <getopt.h>
 
+typedef struct line_st {
+  int valid;
+  unsigned tag;
+} Line;
+
+typedef struct set_st {
+  Line * line;
+  int lruInd;
+} Set;
+
 void printUsage(char * arg) {
   printf("\nUsage: %s [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n\n", arg);
 }
@@ -39,11 +49,28 @@ int main(int argc, char * * argv) {
       return(EXIT_FAILURE);
     }
   }
+  // In case user forgets any of the options
   if (s == 0 || E == 0 || b == 0 || fileStr == NULL) {
-    // In case user forgets any of the options
     printUsage(argv[0]);
     return(EXIT_FAILURE);
   }
+  int S = (1 << s);
+  // Initiates the data structure for the cache
+  Set * set = malloc(S * sizeof(Set));
+  for (int i = 0; i < S; i++) {
+    set[i].line = malloc(E * sizeof(Line));
+    for (int j = 0; j < E; j++) {
+      set[i].line[j].valid = 0;
+    }
+    set[i].lruInd = 0;
+  }
+
+  if (verbose);
   
+  // Frees the data structure before exiting
+  for (int i = 0; i < S; i++) {
+    free(set[i].line);
+  }
+  free(set);
   return(EXIT_SUCCESS);
 }
